26397ebc06cf0f9c5fcd04d22494daa0ed1edda7
gogogm59 <gogogm59@gmail.com>
2021-09-28 11:39:33 +0300

    Add project

---
+++ ArraysExercise/ReverseNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace ReverseNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums[i] = num;
+            }
+            Console.WriteLine(string.Join(" ", nums.Reverse()));
+        }
+    }
+}
---
+++ ArraysExercise/SumEvenNums/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace SumEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(nums.Where(x => x % 2 == 0).Sum());
+        }
+    }
+}
