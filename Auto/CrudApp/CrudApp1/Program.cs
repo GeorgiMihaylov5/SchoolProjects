878f99282f747a220281ba81fa391b9953a8e6ce
Георги Михайлов <gogogm59@outlook.com>
2021-11-04 11:47:00 +0200

    n

---
+++ Auto/CrudApp/CrudApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using CrudApp1.Data;
+using CrudApp1.Presentation;
+using System;
+
+namespace CrudApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoContext context = new AutoContext();
+            Display display = new Display();
+            //
+        }
+    }
+}
