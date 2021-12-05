39728d718253bda96f98a831591313645bdb6f4d
Георги Михайлов <gogogm59@outlook.com>
2021-12-05 14:32:07 +0200

    add project

---
+++ Store2/Store2/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store2
+{
+    public class Product
+    {
+        private string productName;
+        private int quantity;
+        private double price;
+
+        public Product(string productName, int quantity, double price)
+        {
+            this.ProductName = productName;
+            this.Quantity = quantity;
+            this.Price = price;
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("The product name is too short!");
+                }
+                this.productName = value;
+            }
+        }
+        public int Quantity 
+        {
+            get { return this.quantity; }
+            set 
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("The quantity must be a positive number!");
+                }
+                this.quantity = value;
+            }
+        }
+        public double Price 
+        {
+            get { return this.price; } 
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("The price must be a positive number!");
+                }
+                this.price = value;
+            } 
+        }
+
+        public override string ToString()
+        {
+            return $"{this.ProductName} {this.Quantity} cost {this.Price} lv.";
+        }
+        public void Discount()
+        {
+            if (this.Quantity > 10)
+            {
+                this.Price -= this.Price * 10 / 100;
+            }
+        }
+    }
+}
---
+++ Store2/Store2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var products = new List<Product>();
+
+            for (int i = 0; i < n; i++)
+            {
+                //При exсeption задачата продължава, защото е в цикъл
+                try
+                {
+                    string[] input = Console.ReadLine().Split(',').ToArray();
+                    var product = new Product(input[0], int.Parse(input[1]), double.Parse(input[2]));
+
+                    products.Add(product);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (var item in products.OrderByDescending(x => x.ProductName).ThenBy(x => x.Price))
+            {
+                item.Discount();
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
