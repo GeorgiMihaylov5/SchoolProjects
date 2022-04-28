1187846ba7934e2637810872d5898f92d7ee4d44
gogogm59 <gogogm59@gmail.com>
2022-04-28 13:05:07 +0300

    add project

---
+++ UsersDogsBreedsReadyCRUDBul/MoreUsersApp/Models/Dog/DogStatisticVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoreUsersApp.Models.Dog
+{
+    public class DogStatisticVM
+    {
+        public int DogsCount { get; set; }
+    }
+}
